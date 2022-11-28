3c730b1fd37dbc4aadbca66b893a3d8442de8f78
Saida Milena Diaz Castillo <63254311+SaidaKIS@users.noreply.github.com>
2022-11-28 18:47:08 +0000

    Second debug process config file setup

--- config.py
+++ config.py
@@ -5,17 +5,17 @@ from easydict import EasyDict as edict
 
 cfg = edict()
 cfg.root = 'data/Masks_test/' # Raw full IMaX maps (3 for training and 1 for validate)
-cfg.l = 100 #submap dataset
+cfg.l = 10000 #submap dataset
 cfg.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 cfg.seq_len = 5 # Length of the temporal sequence 
 cfg.n_hidden = 16 # initial hidden layers
 cfg.h = 128 #height of the box
 cfg.w = 128 #width of the box
-cfg.batch = 8 
+cfg.batch = 32 
 cfg.bin_classes = ['Intergranular lane', 'Uniform-shaped granules', 'Granules with a dot', 'Granules with a lane',
                 'Complex-shaped granules']
 cfg.channels = 1 # initial channels
-cfg.N_EPOCHS = 5 
+cfg.N_EPOCHS = 100 
 cfg.loss = 'FocalLoss' # 'CrossEntropy', 'FocalLoss', 'mIoU'
 cfg.lr = 3e-4 #Learing rate - inicial 1e-3 test 3e-4
 cfg.dropout = True
